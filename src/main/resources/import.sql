78cb03966d5945e1cb33f21e513b8f11dd09d1ce
harbey-itm <harbeybriceno329728@correo.itm.edu.co>
2022-10-27 19:46:08 -0500

    Adicion Conexion BD Sql Server Microsoft Azure

--- src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-insert into productos (nombre, descripcion, precio) values ('Portatil', 'Equipo de computo Portatil', 1500000);
-insert into productos (nombre, descripcion, precio) values ('Minoxidil', 'Para caida del cabello', 50000);
-insert into productos (nombre, descripcion, precio) values ('Maquina de afietar', 'afeitadora', 150000);
-insert into productos (nombre, descripcion, precio) values ('Minoxidil Spray', 'belleza', 250000);
-insert into productos (nombre, descripcion, precio) values ('Gel Afeitar', 'Producto Hombre', 8000);
